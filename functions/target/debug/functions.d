150f1f309aee967f7c41a979fc1a44c679a105ad
Selhar <selhar@protonmail.com>
2018-01-22 18:01:44 -0300

    chapter 4

---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/selhar/Documents/projetos/rust-book/functions/target/debug/functions: /home/selhar/Documents/projetos/rust-book/functions/src/main.rs
---
+++ strings/target/debug/strings.d
@@ -0,0 +1,1 @@
+/home/selhar/Documents/projetos/rust-book/strings/target/debug/strings: /home/selhar/Documents/projetos/rust-book/strings/src/main.rs
